a3624de24a2d4bfc1018f1b8dc2c47f3fcb5c08b
lusv <viacheslav.lushchinskiy@elca.ch>
2024-11-08 12:01:05 +0100

    initial commit

---
+++ src/main/java/ch/elca/boot_cli_web/BootCliWebApplication.java
@@ -0,0 +1,13 @@
+package ch.elca.boot_cli_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCliWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCliWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/elca/boot_cli_web/BootCliWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.elca.boot_cli_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCliWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
