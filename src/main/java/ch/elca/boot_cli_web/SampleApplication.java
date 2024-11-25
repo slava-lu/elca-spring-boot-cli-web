13913c22fd8dde438888d275b5c6b046f7732003
lusv <viacheslav.lushchinskiy@elca.ch>
2024-11-25 11:02:58 +0100

    refactoring

--- src/main/java/ch/elca/boot_cli_web/SampleApplication.java
+++ src/main/java/ch/elca/boot_cli_web/SampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootCliWebApplication {
+public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootCliWebApplication.class, args);
+		SpringApplication.run(SampleApplication.class, args);
 	}
 
 }
