b02a75c7360ca1d46d0cd58a999ba4819e09c132
lusv <viacheslav.lushchinskiy@elca.ch>
2024-11-27 19:22:25 +0100

    security config changed

--- src/main/java/ch/elca/app/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/ch/elca/app/infrastructure/configuration/SecurityConfig.java
@@ -2,7 +2,9 @@ package ch.elca.app.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /*
@@ -15,10 +17,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/**").permitAll()
-                        .anyRequest().authenticated()
-                );
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth.requestMatchers(
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html",
+                            "index.html",
+                            "/login",
+                            "/logout")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
